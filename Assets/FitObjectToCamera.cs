c40032ad92eb5177c3eda7e0b0d74654c2c949c7
JV <jvscarrion08092006@gmail.com>
2024-11-24 13:57:33 -0300

    Update FitObjectToCamera.cs

--- Assets/FitObjectToCamera.cs
+++ Assets/FitObjectToCamera.cs
@@ -3,8 +3,8 @@ using UnityEngine.Tilemaps;
 
 public class FitGridToCamera : MonoBehaviour
 {
-    public Tilemap tilemap; // Reference to your Tilemap component
-    public Camera mainCamera; // Reference to your Camera component
+    public Tilemap tilemap; 
+    public Camera mainCamera;
 
     void Start()
     {
@@ -24,16 +24,16 @@ public class FitGridToCamera : MonoBehaviour
             return;
         }
 
-        // Get the size of the camera's viewport in world units
+       
         float camHeight = 2f * mainCamera.orthographicSize;
         float camWidth = camHeight * mainCamera.aspect * 1.1f;
 
-        // Calculate the aspect ratio of the tilemap
+       
         BoundsInt bounds = tilemap.cellBounds;
         Vector3Int size = bounds.size;
         float tilemapAspect = (float)size.x / size.y;
 
-        // Adjust the scale of the tilemap to fit the camera's aspect ratio
+       
         float desiredScaleX = camWidth / size.x;
         float desiredScaleY = camHeight / size.y;
 
